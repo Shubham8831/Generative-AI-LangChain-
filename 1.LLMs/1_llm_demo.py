397159c0dafb8b228b2d023a8d31513a7bb97370
Shubham8831 <shubuamsingh2002@gmail.com>
2025-03-05 18:54:17 +0530

    Customized the project

--- 1.LLMs/1_llm_demo.py
+++ 1.LLMs/1_llm_demo.py
@@ -1,10 +1,10 @@
-from langchain_openai import OpenAI
-from dotenv import load_dotenv
+from langchain_openai import OpenAI # this is integration package of langchain and openai
+from dotenv import load_dotenv # loads all the secreat keys form the enviornment files
 
-load_dotenv()
+load_dotenv() # this invokes all the enviornment variables 
 
-llm = OpenAI(model='gpt-3.5-turbo-instruct')
+llm = OpenAI(model='gpt-3.5-turbo-instruct') # here we write which model to communicate with
 
-result = llm.invoke("What is the capital of India")
+result = llm.invoke("What is the capital of India") #  invoke - it helps with communication with llm model
 
 print(result)
